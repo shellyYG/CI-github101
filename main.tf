9891dd7d2299daa712fd940543ffe5fa4497e1c9
shellyyg <chih-ching.yang@whu.edu>
2023-09-11 13:31:23 +0200

    🔧 Add project as env

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.47"
+    }
+  }
+}
+
+locals {
+  project         = "terraform-github-actions"
+}
+
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
   location = "europe-west2"
+  project = local.project
 }
